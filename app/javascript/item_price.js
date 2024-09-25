cf7c435822caab8525713a8f2ef2b4a0ba8a924b
kentouwajima <u.kento6366@gmail.com>
2024-09-25 21:14:57 +0900

    販売手数料と利益の計算の実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,29 @@
+const price = () => {
+
+const priceInput = document.getElementById("item-price");
+
+priceInput.addEventListener("input", () => {
+  const inputValue = priceInput.value;
+  console.log(inputValue);
+
+  // 販売手数料の計算
+  const fee = Math.floor(inputValue * 0.1);
+
+  // 計算結果を表示
+  const addTaxDom = document.getElementById("add-tax-price");
+  addTaxDom.innerHTML = fee; // 計算した手数料を表示
+
+   // 販売利益の計算 (価格 - 販売手数料)
+   const profit = inputValue - fee;
+
+   // 販売利益を表示
+   const profitDom = document.getElementById("profit");
+   profitDom.innerHTML = profit;
+
+})
+
+};
+
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
+
