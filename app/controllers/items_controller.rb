aac0c4b51e431b340c4ebc24ec8e8a0724262a48
kentouwajima <u.kento6366@gmail.com>
2024-10-02 14:35:45 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,9 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
-  
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
+   
+
   def index
     @items = Item.order(created_at: :desc)
   end
@@ -22,10 +25,37 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  private
+  def edit  
+    @item = Item.find(params[:id])
+  end
 
+  def update
+    # @item に対応する商品を見つける
+    @item = Item.find(params[:id])
+
+    # 商品情報を更新し、成功した場合と失敗した場合の処理
+    if @item.update(item_params)
+    # 更新に成功した場合は、商品詳細ページにリダイレクトし、通知を表示
+      redirect_to item_path(@item)
+    else
+    # 更新に失敗した場合は、再度編集ページを表示
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless @item.user == current_user
+      redirect_to action: :index
+    end
   end
 
 end
