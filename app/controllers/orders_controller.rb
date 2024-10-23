cedb8f1dd55be200f26ce15fded65de4a9c734c8
kentouwajima <u.kento6366@gmail.com>
2024-10-23 19:47:27 +0900

    決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,7 @@ class OrdersController < ApplicationController
     @order_address.user_id = current_user.id
     @order_address.item_id = @item.id
     if @order_address.valid?
+       pay_item
        @order_address.save
        redirect_to root_path
     else
@@ -19,7 +20,15 @@ class OrdersController < ApplicationController
 
 private
   def order_address_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number)
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(token: params[:token])
   end
 
+  def pay_item
+    Payjp.api_key = "sk_test_a9b5ef11d8dab7eba3407c9f"  
+      Payjp::Charge.create(
+        amount: @item.price,  
+        card: order_address_params[:token],    
+        currency: 'jpy'                 
+      )
+  end
 end
